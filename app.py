9ae64da1f2b870152eb359311652705980594a49
Baochr <zbbaochangrong.gx@chinatelecom.cn>
2020-09-14 15:24:20 +0800

    2020-9-14

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
@@ -9,13 +9,24 @@ from flask_moment import Moment
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html', current_time=datetime.utcnow())
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            # 闪现消息
+            flash('Looks like you have change your name!')
+        session['name'] = form.name.data
+        # 重定向
+        return redirect(url_for('index'))
+    # 通过会话获取参数
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
@@ -31,6 +42,11 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 if __name__ == '__main__':
