976c7247d2bbef2ed8ca8fcd7951ee4124abc81e
Baochr <zbbaochangrong.gx@chinatelecom.cn>
2020-09-14 13:46:12 +0800

    2020-9-14

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+# -*- coding:utf-8 -*-
+
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
