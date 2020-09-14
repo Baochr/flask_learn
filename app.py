bfc9c668ea475dc1a13eba226a7cad927468763a
Baochr <zbbaochangrong.gx@chinatelecom.cn>
2020-09-14 14:51:58 +0800

    2020-9-14

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 # -*- coding:utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -12,7 +13,12 @@ bootstrap = Bootstrap(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html', current_time=datetime.utcnow())
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
