0ab6839c0dcb701c76edebb3d510cfa3a988cf1d
Baochr <zbbaochangrong.gx@chinatelecom.cn>
2020-09-16 13:47:29 +0800

    2020-9-15

--- hello.py
+++ hello.py
@@ -17,6 +17,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
 
